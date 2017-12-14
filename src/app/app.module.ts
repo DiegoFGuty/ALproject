57ec180a5fee35e824bb3a2033a2a3bae9a6b470
Diego Gutiérrez <diegofgutibol@gmail.com>
2017-12-14 02:46:07 -0500

    Adding material to the project and some other changes on HTML + CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+//import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserModule } from '@angular/platform-browser';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//Imports for using Material Design
+import {
+  MatAutocompleteModule,
+} from '@angular/material';
+
+//import {MatIconModule} from '@angular/material/icon';
+
+//Imports for the application
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+//import { AutocompleteSimpleExample } from './app/autocomplete-simple-example';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FormsModule,
+    // HttpModule,
+  ],
+  exports: [
+//    MatAutocompleteModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
